06f69f31282c84e6642614133c93b279d445a43a
Yuuki Galaxy <galaxy001@gmail.com>
2018-07-05 05:06:45 +0800

    modified:   main.go

--- main.go
+++ main.go
@@ -5,12 +5,10 @@ import (
 	"github.com/pobizhe/flora-kit/flora"
 )
 
-
-
 func main() {
 	var configFile, geoipdb string
 	flag.StringVar(&configFile, "s", "flora.default.conf", "specify surge config file")
 	flag.StringVar(&geoipdb, "d", "geoip.mmdb", "specify geoip db file")
-	flora.Run(configFile, geoipdb)
+	flora.Run(configFile)
 
 }
